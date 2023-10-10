5a1a78cedb8b90086df4cb3661a3c7485e658855
cocoluq <89428024+cocoluq@users.noreply.github.com>
2023-10-11 00:28:16 +0200

    all is* functions added

--- Libft/libft.h
+++ Libft/libft.h
@@ -11,7 +11,12 @@
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
+# include <unistd.h>
+
+// Functions Part 1
+size_t	ft_strlen(const char *s);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
