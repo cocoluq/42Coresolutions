f3df5797908beb71300431fc842d80a37d51b95d
cocoluq <89428024+cocoluq@users.noreply.github.com>
2023-12-20 12:52:26 +0100

    main.c demo 1.5
    all essential functions added

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -10,4 +10,61 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (unsigned char)c)
+		{
+			return ((char *)s);
+		}
+		s++;
+	}
+	if (*s == (unsigned char)c)
+	{
+		return ((char *)s);
+	}
+	return (NULL);
+}
+
+size_t	ft_strlen(const char *s)
+{
+    size_t i;
+
+    i = 0;
+    while (s[i])
+    {
+        i++;
+    }
+    return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dst;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	dst = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!dst)
+	{
+		return (NULL);
+	}
+	while (s1[i])
+	{
+		dst[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		dst[i + j] = s2[j];
+		j++;
+	}
+	dst[i + j] = '\0';
+	return (dst);
+}
 
